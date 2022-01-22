d36e55572def85e5282bea4edec54ebe66e56fc5
Xeeshi <49438298+ZeeshanSalem@users.noreply.github.com>
2022-01-22 23:27:11 +0500

    Soloana Ecrow Completed

--- src/error.rs
+++ src/error.rs
@@ -1,19 +1,25 @@
-// In this file, include  program specific errors
-
 use thiserror::Error;
+
 use solana_program::program_error::ProgramError;
 
-
-#[derive(Error,Debug, Copy, Clone)]
-
+#[derive(Error, Debug, Copy, Clone)]
 pub enum EscrowError {
-    
-    #[derive("Invalid Instruction")]
-    InvalidInstruction
+    /// Invalid instruction
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+    /// Not Rent Exempt
+    #[error("Not Rent Exempt")]
+    NotRentExempt,
+    /// Expected Amount Mismatch
+    #[error("Expected Amount Mismatch")]
+    ExpectedAmountMismatch,
+    /// Amount Overflow
+    #[error("Amount Overflow")]
+    AmountOverflow,
 }
 
 impl From<EscrowError> for ProgramError {
-    fn from(e: EscrowError) -> Self{
-        ProgramError::Custom( e as u32)
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
     }
 }
--- src/state.rs
+++ src/state.rs
@@ -3,17 +3,18 @@
 use solana_program::{
     program_pack :: {IsInitialized, Pack, Sealed},
     pubkey :: Pubkey,
+    program_error::ProgramError,
 };
 
-use arrayref :: {array_mut_ref, array_ref, array_refs, mut_array_refs};
+use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
 
 
-pub struct Escrow{
-    pub is_initialized : bool,
-    pub initialized_pubkey : Pubkey,
-    pub temp_token_account_pubkey : Pubkey,
-    pub initializer_token_to_receive_account_pubkey : Pubkey,
-    pub expected_amount : u64,
+pub struct Escrow {
+    pub is_initialized: bool,
+    pub initializer_pubkey: Pubkey,
+    pub temp_token_account_pubkey: Pubkey,
+    pub initializer_token_to_receive_account_pubkey: Pubkey,
+    pub expected_amount: u64,
 }
 
 impl Sealed for Escrow {
@@ -29,28 +30,28 @@ impl IsInitialized for Escrow {
 impl Pack for Escrow {
     const LEN: usize = 105;
     fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError>{
-        let src = array_ref![src, 0, Escrow::LEN];
 
-        let(
+        let src = array_ref![src, 0, Escrow::LEN];
+        let (
             is_initialized,
             initializer_pubkey,
             temp_token_account_pubkey,
             initializer_token_to_receive_account_pubkey,
             expected_amount,
-
-        ) = array_ref![src, 1, 32 , 32, 32, 8];
-
+        ) = array_refs![src, 1, 32, 32, 32, 8];
         let is_initialized = match is_initialized {
             [0] => false,
             [1] => true,
-            _ => return  Err(ProgramError::InvalidAccountData),
+            _ => return Err(ProgramError::InvalidAccountData),
         };
 
-        Ok(Escrow{
+        Ok(Escrow {
             is_initialized,
-            initializer_pubkey : Pubkey::new_from_array(*initializer_pubkey),
-            temp_token_account_pubkey : Pubkey::new_from_array(*temp_token_account_pubkey),
-            initializer_token_to_receive_account_pubkey: Pubkey::new_from_array(*initializer_token_to_receive_account_pubkey),
+            initializer_pubkey: Pubkey::new_from_array(*initializer_pubkey),
+            temp_token_account_pubkey: Pubkey::new_from_array(*temp_token_account_pubkey),
+            initializer_token_to_receive_account_pubkey: Pubkey::new_from_array(
+                *initializer_token_to_receive_account_pubkey,
+            ),
             expected_amount: u64::from_le_bytes(*expected_amount),
         })
     }
